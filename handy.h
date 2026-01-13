bf703e563e21ec7381ebb515616b867a90edf2e2
Karl Williamson <khw@cpan.org>
2026-01-13 05:55:58 -0700

    Create CC_bit_(), isCC_by_bit_()
    CC_bit_(), not part of the public API, converts a class number, as
    defined in handy.h, into a bit pattern corresponding to what
    PL_charclass[] uses to signify if a character is a member of the given
    class.
    
    isCC_by_bit_(),not part of the public API, returns a boolean as to
    whether or not a character is a member of a class or classes, as
    determined by PL_charclass[].  Besides the character, it takes a bit
    argument, as given by the return of CC_bit_.
    
    I think it is instructive as to how these macros came to be.  I created
    PL_charclass[] a long time ago to speed up character classification, so
    that instead of several if..else.. statements, one could do a simple
    array lookup to see if a character is a \w, for example.
    
    I envisioned this as being used for things like isWORDCHAR, where the
    class number was baked-in to the macro name.
    
    Over time, cases where the class number was not known until runtime
    presented themselves.  It was easy to just use the base macros for that,
    and everything worked.  But what I neglected, and no one caught, is that
    there is a runtime cost to deriving the bit position from the class
    number.   But Tony Cook noticed recently that grok_bin_oct_hex()
    rederives that same value multiple times.
    
    So these macros have been invented to avoid that multiple runtime cost.

--- handy.h
+++ handy.h
@@ -1600,11 +1600,30 @@ END_EXTERN_C
 
     /* The 1U keeps Solaris from griping when shifting sets the uppermost bit */
 #   define CC_mask_(classnum) (1U << (classnum))
+#   define CC_bit_(classnum)  CC_mask_(classnum)
+
+/*
+=for apidoc_section $classification
+=for apidoc Cm|U32|CC_bit_|U8 c|U8 classnum
+
+This translates C<classnum> into a bit pattern to use in conjunction with
+entries in C<PL_charclass[]>, and operations such is C<L</isCC_by_bit_>>.
+C<classnum> is one of the classes defined in F<handy.h> for this purpose.
+
+=for apidoc Cm|bool|isCC_by_bit_|U8 c|U32 bit_pattern
+
+Returns a boolean as to whether or not the character C<c> in the Latin1 range
+is a member of the class(es) given by C<bit_pattern>, as formed by
+C<L</CC_bit_>>.
+
+=cut
+*/
+#  define isCC_by_bit_(c, bit_pattern)                                  \
+       (FITS_IN_8_BITS(c) && (PL_charclass[(U8) (c)] & (bit_pattern)))
 
     /* For internal core Perl use only: the base macro for defining macros like
      * isALPHA */
-#   define generic_isCC_(c, classnum)                                       \
-       (FITS_IN_8_BITS(c) && (PL_charclass[(U8) (c)] & CC_mask_(classnum)))
+#   define generic_isCC_(c, classnum)  isCC_by_bit_(c, CC_mask_(classnum))
 
     /* The mask for the _A versions of the macros; it just adds in the bit for
      * ASCII. */
